69f1f895b094efa091d7886d55269ea9e79245bc
Lai99 <aaaxaaa99@gmail.com>
2016-05-23 18:11:31 +0800

    v0.12

--- pttcrawler/spiders/ptt.py
+++ pttcrawler/spiders/ptt.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from datetime import datetime
 import logging
+from pttcrawler.items import CrawlerItem
 
 from scrapy.http import FormRequest
 
@@ -24,7 +26,7 @@ class PttSpider(scrapy.Spider):
             if self._retries < PttSpider.MAX_RETRY:
                 self._retries += 1
                 logging.warning('retry {} times...'.format(self._retries))
-                return FormRequest.from_response(response,
+                yield FormRequest.from_response(response,
                                                 formdata={'yes': 'yes'},
                                                 callback=self.parse)
             else:
@@ -38,34 +40,33 @@ class PttSpider(scrapy.Spider):
             self._pages += 1
             for href in response.css('.r-ent > div.title > a::attr(href)'):
                 url = response.urljoin(href.extract())
-                return scrapy.Request(url, callback=self.parse_post)
+                yield scrapy.Request(url, callback=self.parse_post)
 
             if self._pages < PttSpider.MAX_PAGES:
                 next_page = response.xpath(
-                    '//div[@id="action-bar-container"]//a[contains(text(), "上頁")]/@href')
+                    u'//div[@id="action-bar-container"]//a[contains(text(), "上頁")]/@href')
                 if next_page:
                     url = response.urljoin(next_page[0].extract())
                     logging.warning('follow {}'.format(url))
-                    return scrapy.Request(url, self.parse)
+                    yield scrapy.Request(url, self.parse)
                 else:
                     logging.warning('no next page')
             else:
                 logging.warning('max pages reached')
 
     def parse_post(self, response):
-        item = PostItem()
+        item = CrawlerItem()
         item['title'] = response.xpath(
             '//meta[@property="og:title"]/@content')[0].extract()
         item['author'] = response.xpath(
-            '//div[@class="article-metaline"]/span[text()="作者"]/following-sibling::span[1]/text()')[
+            u'//div[@class="article-metaline"]/span[text()="作者"]/following-sibling::span[1]/text()')[
             0].extract().split(' ')[0]
         datetime_str = response.xpath(
-            '//div[@class="article-metaline"]/span[text()="時間"]/following-sibling::span[1]/text()')[
+            u'//div[@class="article-metaline"]/span[text()="時間"]/following-sibling::span[1]/text()')[
             0].extract()
         item['date'] = datetime.strptime(datetime_str, '%a %b %d %H:%M:%S %Y')
 
-        item['content'] = response.xpath('//div[@id="main-content"]/text()')[
-            0].extract()
+        item['content'] = response.xpath('//div[@id="main-content"]/text()')[0].extract()
 
         comments = []
         total_score = 0
@@ -74,9 +75,9 @@ class PttSpider(scrapy.Spider):
             push_user = comment.css('span.push-userid::text')[0].extract()
             push_content = comment.css('span.push-content::text')[0].extract()
 
-            if '推' in push_tag:
+            if u'推' in push_tag:
                 score = 1
-            elif '噓' in push_tag:
+            elif u'噓' in push_tag:
                 score = -1
             else:
                 score = 0
@@ -91,4 +92,4 @@ class PttSpider(scrapy.Spider):
         item['score'] = total_score
         item['url'] = response.url
 
-        return item
+        yield item
